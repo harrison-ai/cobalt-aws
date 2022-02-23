daf6f6124f0fb0745c93ce8b2caae57fc70f17dd
Tim Leslie <tim.leslie@harrison.ai>
2022-02-24 08:46:51 +1100

    Add s3::list_objects to stream an object listing from a bucket (#16)

--- src/localstack.rs
+++ src/localstack.rs
@@ -24,49 +24,136 @@ pub(crate) fn get_endpoint_uri() -> Result<Option<Uri>> {
 }
 
 #[cfg(test)]
+pub mod test_utils {
+    use super::*;
+    use reqwest;
+    use serde::Deserialize;
+    use std::thread::sleep;
+    use std::time::{Duration, Instant};
+
+    #[derive(Deserialize, Debug)]
+    struct Health {
+        features: Option<Feature>,
+    }
+    #[derive(Deserialize, Debug)]
+    #[allow(non_snake_case)]
+    struct Feature {
+        initScripts: Option<String>,
+    }
+
+    /// This function polls the <localstack_url>/health endpoint and waits for the { features: initScripts } value to
+    /// be "initialized".
+    ///
+    /// We poll at one second intervals and timeout after 1 minute.
+    ///
+    /// This API doesn't seem to be publicly documented, but the code can be seen here:
+    ///
+    /// https://github.com/localstack/localstack/blob/b21178e1d62bfd784058496ac9d34d91c11bd329/bin/docker-entrypoint.sh#L54
+    ///
+    /// See also: https://github.com/localstack/localstack/pull/4770/files
+    ///
+    /// # Panic
+    ///
+    /// This function will panic on any kind of failure
+    pub async fn wait_for_localstack() {
+        // If tests are being run locally (e.g. not from within docker) then we
+        // expect localstack to be available at localhost:4566.
+        if env::var("LOCALSTACK_HOSTNAME").is_err() {
+            env::set_var("LOCALSTACK_HOSTNAME", "localhost");
+        }
+        // Our local stack configuration is setup to run as ap-southeast-2,
+        // so we explicitly set this here.
+        env::set_var("AWS_DEFAULT_REGION", "ap-southeast-2");
+
+        let uri = get_endpoint_uri().unwrap().unwrap();
+        let now = Instant::now();
+        loop {
+            match reqwest::get(&format!("{:?}health", uri)).await {
+                Err(_) => {
+                    println!("Localstack URL: {:#?}", uri);
+                    panic!("Unable to connect to localstack. To run localstack locally, run `docker-compose up -d`");
+                }
+                Ok(response) => {
+                    let health: Health = response.json().await.unwrap();
+                    if let Some(features) = health.features {
+                        if let Some(s) = features.initScripts {
+                            if s == "initialized" {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            if now.elapsed().as_secs() > 60 {
+                println!("Localstack URL: {:#?}", uri);
+                panic!("Timed out while waiting for localstack to initialise!")
+            }
+            sleep(Duration::new(1, 0));
+        }
+    }
+}
+
+#[cfg(test)]
 mod test {
     use super::*;
 
     use serial_test::serial;
 
+    /// Run get_endpoint_uri() with a given host and port in the environment, resetting
+    /// the environment to its original state afterwards.
+    fn safe_get_endpoint_uri(host: Option<String>, port: Option<String>) -> Result<Option<Uri>> {
+        let orig_host = env::var("LOCALSTACK_HOSTNAME");
+        let orig_port = env::var("EDGE_PORT");
+
+        match host {
+            Some(host) => env::set_var("LOCALSTACK_HOSTNAME", host),
+            None => env::remove_var("LOCALSTACK_HOSTNAME"),
+        }
+
+        match port {
+            Some(port) => env::set_var("EDGE_PORT", port),
+            None => env::remove_var("EDGE_PORT"),
+        }
+
+        let uri = get_endpoint_uri();
+
+        match orig_host {
+            Ok(host) => env::set_var("LOCALSTACK_HOSTNAME", host),
+            Err(_) => env::remove_var("LOCALSTACK_HOSTNAME"),
+        }
+        match orig_port {
+            Ok(port) => env::set_var("EDGE_PORT", port),
+            Err(_) => env::remove_var("EDGE_PORT"),
+        }
+
+        uri
+    }
+
     #[test]
     #[serial]
     fn test_get_localstack_endpoint_empty() {
-        let uri = get_endpoint_uri().unwrap();
+        let uri = safe_get_endpoint_uri(None, None).unwrap();
         assert_eq!(uri, None);
     }
 
     #[test]
     #[serial]
     fn test_get_localstack_endpoint_host() {
-        env::set_var("LOCALSTACK_HOSTNAME", "test_hostname");
-        let uri = get_endpoint_uri().unwrap();
-        env::remove_var("LOCALSTACK_HOSTNAME");
-
+        let uri = safe_get_endpoint_uri(Some("test_hostname".into()), None).unwrap();
         assert_eq!(uri, Some(Uri::from_static("http://test_hostname:4566")));
     }
 
     #[test]
     #[serial]
     fn test_get_localstack_endpoint_host_port() {
-        env::set_var("LOCALSTACK_HOSTNAME", "test_hostname");
-        env::set_var("EDGE_PORT", "1234");
-        let uri = get_endpoint_uri().unwrap();
-        env::remove_var("LOCALSTACK_HOSTNAME");
-        env::remove_var("EDGE_PORT");
-
+        let uri = safe_get_endpoint_uri(Some("test_hostname".into()), Some("1234".into())).unwrap();
         assert_eq!(uri, Some(Uri::from_static("http://test_hostname:1234")));
     }
 
     #[test]
     #[serial]
     fn test_get_localstack_endpoint_bad_uri() {
-        env::set_var("LOCALSTACK_HOSTNAME", "bad:host");
-        env::set_var("EDGE_PORT", "not-a-number");
-        let uri = get_endpoint_uri();
-        env::remove_var("LOCALSTACK_HOSTNAME");
-        env::remove_var("EDGE_PORT");
-
+        let uri = safe_get_endpoint_uri(Some("bad:host".into()), Some("not-a-number".into()));
         match uri {
             Ok(uri) => Err(format!("Expected error, recieved {:?}", uri)),
             Err(e) => {
--- src/s3.rs
+++ src/s3.rs
@@ -1,7 +1,11 @@
 //! A collection of wrappers around the [aws_sdk_s3](https://docs.rs/aws-sdk-s3/latest/aws_sdk_s3/) crate.
 
 use anyhow::Result;
-use aws_sdk_s3::{config, Endpoint};
+use aws_sdk_s3::error::ListObjectsV2Error;
+use aws_sdk_s3::types::SdkError;
+use aws_sdk_s3::{config, model, Endpoint};
+use futures::stream;
+use futures::stream::{Stream, TryStreamExt};
 
 use crate::localstack;
 
@@ -54,18 +58,193 @@ pub fn get_client(shared_config: &aws_config::Config) -> Result<Client> {
     Ok(Client::from_conf(builder.build()))
 }
 
+/// Perform a bucket listing, returning a stream of results.
+///
+/// # Example
+///
+/// ```no_run
+/// use aws_config;
+/// use cobalt_aws::s3::{get_client, list_objects};
+/// use futures::TryStreamExt;
+///
+/// # tokio_test::block_on(async {
+/// let shared_config = aws_config::load_from_env().await;
+/// let client = get_client(&shared_config).unwrap();
+/// let mut objects = list_objects(&client, "my-bucket", Some("prefix".into()));
+/// while let Some(item) = objects.try_next().await.unwrap() {
+///     println!("{:?}", item);
+/// }
+/// # })
+/// ```
+///
+///  # Implementation details
+///
+/// This function uses the [ListObjectsV2](https://docs.aws.amazon.com/AmazonS3/latest/API/API_ListObjectsV2.html)
+/// API and performs pagination to ensure all objects are returned.
+pub fn list_objects(
+    client: &Client,
+    bucket: impl Into<String>,
+    prefix: Option<String>,
+) -> impl Stream<Item = Result<model::Object, SdkError<ListObjectsV2Error>>> + Unpin {
+    let req = client
+        .list_objects_v2()
+        .bucket(bucket)
+        .set_prefix(prefix)
+        .into_paginator();
+    req.send()
+        .map_ok(|list_objs| {
+            stream::iter(
+                list_objs
+                    .contents
+                    .unwrap_or_default() // An empty bucket comes back as None, rather than an empty vector
+                    .into_iter()
+                    .map(Ok),
+            )
+        })
+        .try_flatten()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     use aws_config;
+    use futures::TryStreamExt;
     use serial_test::serial;
+    use std::error::Error;
     use tokio;
 
     #[tokio::test]
     #[serial]
     async fn test_get_client() {
-        let config = aws_config::load_from_env().await;
-        get_client(&config).unwrap();
+        let shared_config = aws_config::load_from_env().await;
+        get_client(&shared_config).unwrap();
+    }
+
+    async fn localstack_test_client() -> Client {
+        localstack::test_utils::wait_for_localstack().await;
+        let shared_config = aws_config::load_from_env().await;
+        get_client(&shared_config).unwrap()
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_non_existant_bucket() {
+        let client = localstack_test_client().await;
+
+        let stream = list_objects(&client, "non-existant-bucket", None);
+        let e = stream.try_collect::<Vec<_>>().await.unwrap_err();
+        assert!(matches!(
+            e.source()
+                .unwrap()
+                .downcast_ref::<ListObjectsV2Error>()
+                .unwrap()
+                .kind,
+            aws_sdk_s3::error::ListObjectsV2ErrorKind::NoSuchBucket(_)
+        ))
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_empty_bucket() {
+        let client = localstack_test_client().await;
+
+        let stream = list_objects(&client, "empty-bucket", None);
+        let results = stream.try_collect::<Vec<_>>().await.unwrap();
+        assert_eq!(results, vec![]);
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_no_prefix() {
+        let client = localstack_test_client().await;
+
+        let stream = list_objects(&client, "test-bucket", None);
+        let mut results = stream.try_collect::<Vec<_>>().await.unwrap();
+        results.sort_by_cached_key(|x| x.size);
+        assert_eq!(results.len(), 2503);
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_with_prefix() {
+        let client = localstack_test_client().await;
+
+        let stream = list_objects(&client, "test-bucket", Some("some-prefix".into()));
+        let results = stream.try_collect::<Vec<_>>().await.unwrap();
+        assert_eq!(results.len(), 2);
+        assert_eq!(
+            results[0].key,
+            Some("some-prefix/nested-prefix/nested.txt".into())
+        );
+        assert_eq!(results[0].size, 12);
+        assert_eq!(results[1].key, Some("some-prefix/prefixed.txt".into()));
+        assert_eq!(results[1].size, 14);
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_with_prefix_slash() {
+        let client = localstack_test_client().await;
+
+        let stream = list_objects(&client, "test-bucket", Some("some-prefix/".into()));
+        let results = stream.try_collect::<Vec<_>>().await.unwrap();
+        assert_eq!(results.len(), 2);
+        assert_eq!(
+            results[0].key,
+            Some("some-prefix/nested-prefix/nested.txt".into())
+        );
+        assert_eq!(results[0].size, 12);
+        assert_eq!(results[1].key, Some("some-prefix/prefixed.txt".into()));
+        assert_eq!(results[1].size, 14);
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_with_nested_prefix() {
+        let client = localstack_test_client().await;
+
+        let stream = list_objects(
+            &client,
+            "test-bucket",
+            Some("some-prefix/nested-prefix".into()),
+        );
+        let results = stream.try_collect::<Vec<_>>().await.unwrap();
+        assert_eq!(results.len(), 1);
+        assert_eq!(
+            results[0].key,
+            Some("some-prefix/nested-prefix/nested.txt".into())
+        );
+        assert_eq!(results[0].size, 12);
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_with_partial_prefix() {
+        let client = localstack_test_client().await;
+
+        let stream = list_objects(&client, "test-bucket", Some("empty-pre".into()));
+        let results = stream.try_collect::<Vec<_>>().await.unwrap();
+        assert_eq!(results.len(), 0);
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_with_empty_prefix() {
+        let client = localstack_test_client().await;
+
+        let stream = list_objects(&client, "test-bucket", Some("empty-prefix".into()));
+        let results = stream.try_collect::<Vec<_>>().await.unwrap();
+        assert_eq!(results.len(), 0);
+    }
+
+    #[tokio::test]
+    #[serial]
+    async fn test_with_multiple_pages() {
+        let client = localstack_test_client().await;
+
+        let stream = list_objects(&client, "test-bucket", Some("multi-page".into()));
+        let results = stream.try_collect::<Vec<_>>().await.unwrap();
+        assert_eq!(results.len(), 2500);
     }
 }
