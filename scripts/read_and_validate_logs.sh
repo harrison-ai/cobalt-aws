ab9190226ccd2f2e13ee43292997bec259ed391d
Tanya Srinidhi <tanu.srinidhi@gmail.com>
2022-07-08 13:30:25 +1000

    Fix variable name

--- scripts/read_and_validate_logs.sh
+++ scripts/read_and_validate_logs.sh
@@ -21,16 +21,17 @@ if [ -z "$STREAM_NAME" ] || [ "$STREAM_NAME" = "null" ]; then
     exit 1
 fi
 
-export LOGS=$($AWSLOCAL logs get-log-events --log-group-name /aws/lambda/$EXAMPLE --log-stream-name "$STREAM_NAME" --start-from-head \
- | jq -r '.events[].message')
+export AWS_LOGS=`$AWSLOCAL logs get-log-events --log-group-name /aws/lambda/$EXAMPLE --log-stream-name "$STREAM_NAME" --start-from-head \
+ | jq -r '.events[].message'`
 
-log=$(echo $LOGS | sed -e 's/.*LATEST\(.*\)END.*/\1/' |  sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
+# Extracting the message and removing ASCII color tags
+log=$(echo $AWS_LOGS | sed -e 's/.*LATEST\(.*\)END.*/\1/' |  sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g')
 if [ -z "$log" ] || [ "$log" = "null" ]; then
   echo "Empty logs"
   exit 1
 fi
 
-message=$(echo $message | jq -r ".fields.message")
+message=$(echo $log | jq -r ".fields.message")
 if [ "$message" = "$EXPECTED_MESSAGE" ]; then
   echo "Test passed! Found expected message: $message"
   exit 0
